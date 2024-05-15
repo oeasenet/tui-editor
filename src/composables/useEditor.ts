e03998d6a09ef11e02c8dc95a71d9836694f5313
Cédric Dinont <cedric@ecole2600.com>
2024-05-15 12:36:27 +0200

    do not use strings to reference plugins

--- src/composables/useEditor.ts
+++ src/composables/useEditor.ts
@@ -1,52 +1,55 @@
-import '@toast-ui/editor/dist/i18n/ar'
-import '@toast-ui/editor/dist/i18n/cs-cz'
-import '@toast-ui/editor/dist/i18n/de-de'
-import '@toast-ui/editor/dist/i18n/es-es'
-import '@toast-ui/editor/dist/i18n/fi-fi'
-import '@toast-ui/editor/dist/i18n/fr-fr'
-import '@toast-ui/editor/dist/i18n/gl-es'
-import '@toast-ui/editor/dist/i18n/it-it'
-import '@toast-ui/editor/dist/i18n/ja-jp'
-import '@toast-ui/editor/dist/i18n/ko-kr'
-import '@toast-ui/editor/dist/i18n/nb-no'
-import '@toast-ui/editor/dist/i18n/nl-nl'
-import '@toast-ui/editor/dist/i18n/pl-pl'
-import '@toast-ui/editor/dist/i18n/ru-ru'
-import '@toast-ui/editor/dist/i18n/sv-se'
-import '@toast-ui/editor/dist/i18n/tr-tr'
-import '@toast-ui/editor/dist/i18n/uk-ua'
-import '@toast-ui/editor/dist/i18n/zh-cn'
-import '@toast-ui/editor/dist/i18n/zh-tw'
-import '@toast-ui/editor/dist/i18n/zh-tw'
+import "@toast-ui/editor/dist/i18n/ar";
+import "@toast-ui/editor/dist/i18n/cs-cz";
+import "@toast-ui/editor/dist/i18n/de-de";
+import "@toast-ui/editor/dist/i18n/es-es";
+import "@toast-ui/editor/dist/i18n/fi-fi";
+import "@toast-ui/editor/dist/i18n/fr-fr";
+import "@toast-ui/editor/dist/i18n/gl-es";
+import "@toast-ui/editor/dist/i18n/it-it";
+import "@toast-ui/editor/dist/i18n/ja-jp";
+import "@toast-ui/editor/dist/i18n/ko-kr";
+import "@toast-ui/editor/dist/i18n/nb-no";
+import "@toast-ui/editor/dist/i18n/nl-nl";
+import "@toast-ui/editor/dist/i18n/pl-pl";
+import "@toast-ui/editor/dist/i18n/ru-ru";
+import "@toast-ui/editor/dist/i18n/sv-se";
+import "@toast-ui/editor/dist/i18n/tr-tr";
+import "@toast-ui/editor/dist/i18n/uk-ua";
+import "@toast-ui/editor/dist/i18n/zh-cn";
+import "@toast-ui/editor/dist/i18n/zh-tw";
+import "@toast-ui/editor/dist/i18n/zh-tw";
 
-import { Ref } from 'vue'
-import Editor, { EditorType, PreviewStyle } from '@toast-ui/editor'
-import { ToolbarItemOptions } from '@toast-ui/editor/types/ui'
-import { PluginName, mapPlugins } from '../utils/TuiPlugins'
+import { Ref } from "vue";
+import Editor, { EditorType, PreviewStyle } from "@toast-ui/editor";
+import { ToolbarItemOptions } from "@toast-ui/editor/types/ui";
+import { PluginName, mapPlugins } from "../utils/TuiPlugins";
 
-export type HookCallback = (url: string, text?: string) => void
-export type AddImageBlobHook = (blob: Blob | File, callback: HookCallback) => void
+export type HookCallback = (url: string, text?: string) => void;
+export type AddImageBlobHook = (
+    blob: Blob | File,
+    callback: HookCallback
+) => void;
 
 interface Options {
-    initialValue: string
+    initialValue: string;
 
-    height?: string
-    hideModeSwitch?: boolean
-    initialEditType?: EditorType
-    language?: string
-    plugins?: PluginName[]
-    previewStyle?: PreviewStyle
-    toolbarItems?: (string | ToolbarItemOptions)[][]
-    usageStatistics?: boolean
-    useCommandShortcut?: boolean
+    height?: string;
+    hideModeSwitch?: boolean;
+    initialEditType?: EditorType;
+    language?: string;
+    plugins?: any[];
+    previewStyle?: PreviewStyle;
+    toolbarItems?: (string | ToolbarItemOptions)[][];
+    usageStatistics?: boolean;
+    useCommandShortcut?: boolean;
 
-    onChange?: (e: Editor) => void
-    addImageBlobHook?: AddImageBlobHook
+    onChange?: (e: Editor) => void;
+    addImageBlobHook?: AddImageBlobHook;
 }
 
 export default (elRef: Ref<HTMLElement | null>, options: Options) => {
     if (!elRef.value) {
-        throw new Error('Reference to the element is set')
+        throw new Error("Reference to the element is set");
     }
 
     const e: Editor = new Editor({
@@ -57,9 +60,7 @@ export default (elRef: Ref<HTMLElement | null>, options: Options) => {
         initialEditType: options.initialEditType,
         initialValue: options.initialValue,
         language: options.language,
-        plugins: options.plugins !== undefined
-            ? mapPlugins(options.plugins)
-            : undefined,
+        plugins: options.plugins,
         previewStyle: options.previewStyle,
         toolbarItems: options.toolbarItems,
         usageStatistics: options.usageStatistics,
@@ -68,14 +69,12 @@ export default (elRef: Ref<HTMLElement | null>, options: Options) => {
         events: {
             change: options.onChange
                 ? () => options.onChange && options.onChange(e)
-                : () => { },
+                : () => {},
         },
         hooks: {
             addImageBlobHook: options.addImageBlobHook,
-        }
-    })
+        },
+    });
 
-    return e
-}
-
-
+    return e;
+};
--- src/composables/useViewer.ts
+++ src/composables/useViewer.ts
@@ -1,11 +1,11 @@
-import Viewer from '@toast-ui/editor/dist/toastui-editor-viewer'
-import { Ref } from 'vue'
-import { PluginName, mapPlugins } from '../utils/TuiPlugins'
+import Viewer from "@toast-ui/editor/dist/toastui-editor-viewer";
+import { Ref } from "vue";
+import { PluginName, mapPlugins } from "../utils/TuiPlugins";
 
 interface Options {
-    initialValue?: string
+    initialValue?: string;
 
-    plugins?: PluginName[];
+    plugins?: any[];
     // extendedAutolinks?: ExtendedAutolinks;
     // linkAttributes?: LinkAttributes;
     // referenceDefinition?: boolean;
@@ -16,16 +16,14 @@ interface Options {
 
 export default (elRef: Ref<HTMLElement | null>, options: Options) => {
     if (!elRef.value) {
-        throw new Error('Reference to the element is set')
+        throw new Error("Reference to the element is set");
     }
 
     const e: Viewer = new Viewer({
         el: elRef.value,
-        plugins: options.plugins !== undefined
-            ? mapPlugins(options.plugins)
-            : undefined,
+        plugins: options.plugins,
         initialValue: options.initialValue,
-    })
+    });
 
-    return e
-}
+    return e;
+};
